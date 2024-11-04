78cf1f8ff8aa1538155931bc1c98ec94021f1a6e
Yu Chen <rabbit19981023@gmail.com>
2024-11-04 17:30:00 +0800

    example: add simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use async_openai::{
+    config::OpenAIConfig,
+    types::{
+        ChatCompletionRequestSystemMessage, ChatCompletionRequestUserMessage,
+        CreateChatCompletionRequestArgs,
+    },
+    Client,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::with_config(OpenAIConfig::new().with_api_base("http://localhost:4000"));
+    let model = "gpt-3.5-turbo";
+
+    chat(&client, model).await
+}
+
+async fn chat(client: &Client<OpenAIConfig>, model: &str) -> Result<(), Box<dyn Error>> {
+    let request = CreateChatCompletionRequestArgs::default()
+        .model(model)
+        .messages([
+            ChatCompletionRequestSystemMessage::from("You are a helpful assistant.").into(),
+            ChatCompletionRequestUserMessage::from("who won the world series in 2020?").into(),
+        ])
+        .build()?;
+
+    let response = client.chat().create(request).await?;
+
+    for choice in response.choices {
+        match choice.message.content {
+            Some(content) => println!(
+                "[{}] Role: {}, Response: {}",
+                choice.index, choice.message.role, content
+            ),
+            None => println!("{}: <empty>", choice.index),
+        }
+    }
+
+    Ok(())
 }
