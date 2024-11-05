a52c21c0f91a5b63b0fdf3a2ec406e9041d9afe5
Yu Chen <rabbit19981023@gmail.com>
2024-11-05 18:25:26 +0800

    example: add simple qdrant example & use anyhow to simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::error::Error;
-
+use anyhow::Result;
 use async_openai::{
     config::OpenAIConfig,
     types::{
@@ -8,16 +7,38 @@ use async_openai::{
     },
     Client,
 };
+use qdrant_client::{qdrant::CreateCollectionBuilder, Qdrant};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::with_config(OpenAIConfig::new().with_api_base("http://localhost:4000"));
+async fn main() -> Result<()> {
+    let llm = Client::with_config(OpenAIConfig::new().with_api_base("http://localhost:4000"));
     let model = "gpt-3.5-turbo";
 
-    chat(&client, model).await
+    let qdrant = Qdrant::from_url("http://localhost:6334").build()?;
+
+    chat(&llm, model).await?;
+
+    embedding(&qdrant).await?;
+
+    Ok(())
 }
 
-async fn chat(client: &Client<OpenAIConfig>, model: &str) -> Result<(), Box<dyn Error>> {
+async fn embedding(qdrant: &Qdrant) -> Result<()> {
+    let collection_name = "test";
+
+    {
+        qdrant.delete_collection(collection_name).await?;
+        qdrant
+            .create_collection(CreateCollectionBuilder::new(collection_name))
+            .await?;
+    }
+
+    dbg!(qdrant.collection_info("test").await?);
+
+    Ok(())
+}
+
+async fn chat(llm: &Client<OpenAIConfig>, model: &str) -> Result<()> {
     let request = CreateChatCompletionRequestArgs::default()
         .model(model)
         .messages([
@@ -26,7 +47,7 @@ async fn chat(client: &Client<OpenAIConfig>, model: &str) -> Result<(), Box<dyn 
         ])
         .build()?;
 
-    let response = client.chat().create(request).await?;
+    let response = llm.chat().create(request).await?;
 
     for choice in response.choices {
         match choice.message.content {
